e84aa55844fee31db755f8ddab8a4ca6070bc246
pavelFaraday <devstudio.corp@gmail.com>
2020-04-22 20:56:15 +0400

    ''upload

---
+++ babel/src/index.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+// [0-9] === \d
+// ^\d{11}$ --- means only: 12345678901.  !not: abc12345678901 or abc12345678901jnm;   
+// Select all inputs for validation
+var inputs = document.querySelectorAll('input'); // Validation scrip here:
+
+var patterns = {
+  telephone: /^\d{11}$/
+}; // attach keyup events to inputs
+
+inputs.forEach(function (input) {
+  input.addEventListener('keyup', function (e) {
+    console.log(e.target.attributes.name.value);
+  });
+});
--- validation.js
+++
@@ -1,8 +0,0 @@
-// [0-9] === \d
-// ^\d{11}$ --- means only: 12345678901.  !not: abc12345678901 or abc12345678901jnm;   
-
-
-// Validation scrip here:
-var patterns = {
-    telephone: /^\d{11}$/
-};
