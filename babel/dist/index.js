aa7ab5de474aa66aa97b7b180a01b30dc1d36dea
pavelFaraday <devstudio.corp@gmail.com>
2020-04-23 00:01:56 +0400

    finish telephone, username, password and slug REGEXes

--- babel/dist/index.js
+++ babel/dist/index.js
@@ -2,7 +2,10 @@
 
 var inputs = document.querySelectorAll('input');
 var patterns = {
-  telephone: /^\d{11}$/
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/
 };
 
 function validate(field, regex) {
